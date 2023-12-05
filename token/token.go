0572cc7c5647861afae86e92eaa34cd1d75e29ca
wallchristopher <wall.christopher.d@gmail.com>
2023-12-05 10:28:34 -0600

    adding more operators and test cases

--- token/token.go
+++ token/token.go
@@ -17,8 +17,14 @@ const (
 	INT   = "INT"   // 123456
 
 	// Operators
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+	LT       = "<"
+	GT       = ">"
 
 	// Delimiters
 	COMMA     = ","
