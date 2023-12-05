18e1861a0e63008c65fca6e0870006449a6ad3ff
wallchristopher <wall.christopher.d@gmail.com>
2023-12-05 11:16:51 -0600

    adding repl for interactive prompt and the main function

---
+++ repl/repl.go
@@ -0,0 +1,31 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"monkey-interpreter/lexer"
+	"monkey-interpreter/token"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Printf(PROMPT)
+		scanned := scanner.Scan()
+
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		lex := lexer.New(line)
+
+		for tok := lex.NextToken(); tok.Type != token.EOF; tok = lex.NextToken() {
+			fmt.Printf("%+v\n", tok)
+		}
+	}
+}
