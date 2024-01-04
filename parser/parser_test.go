28daec062d0c06a252711a03a4afe5fb251892f4
wallchristopher <wall.christopher.d@gmail.com>
2024-01-04 10:28:42 -0600

    parsing return statement and adding testing

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -77,3 +77,34 @@ func testLetStatement(t *testing.T, s ast.Statement,
 	}
 	return true
 }
+
+func TestReturnStatements(t *testing.T) {
+	input := `
+return 5;
+return 10;
+return 993322;
+`
+
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements. got=%d",
+			len(program.Statements))
+	}
+
+	for _, stmt := range program.Statements {
+		returnStmt, ok := stmt.(*ast.ReturnStatement)
+		if !ok {
+			t.Errorf("s not *ast.ReturnStatement. got=%T", stmt)
+			continue
+		}
+		if returnStmt.TokenLiteral() != "return" {
+			t.Errorf("returnStmt.TokenLiteral not 'return'. got=%q",
+				returnStmt.TokenLiteral())
+		}
+	}
+}
